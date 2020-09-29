a3214b1f5ffca5b6a3e7afa6d5ed07ee6f3107e3
Miloud Bouazza <mbouazza@sonora11.univ-lr.fr>
2020-09-29 16:06:20 +0200

    debut du site

---
+++ script.js
@@ -0,0 +1,66 @@
+window.onload = function() {
+    // ouverture fermeture menu
+			const body = document.querySelector('body');
+            const menu = document.querySelector('.menu-icon');
+			menu.addEventListener('click', function () {
+                body.classList.toggle('nav-active');
+			});
+    
+    // changement de couleur:
+        const switchBarre = document.getElementById('switch-barre');
+        const switchCercle = document.getElementById('switch-cercle');
+        
+
+        const logo = this.document.getElementById('logo');
+        const root = document.documentElement;
+
+        const imgsReseau = this.document.querySelectorAll('.img-reseau');
+        switchCercle.addEventListener('click', function (){
+            // changer couleur logo et
+            //changer les valeurs des variable couleurs
+            if(getComputedStyle(root).getPropertyValue('--couleur-principale').includes('#000')){
+                // passage en blanc
+                root.style.setProperty('--couleur-principale','#fff');
+                root.style.setProperty('--couleur-secondaire','#000');
+                root.style.setProperty('--couleur-ombre','rgba(0,0,0,0.1)');
+                logo.style.filter = 'invert(1)'; 
+                // imgsReseau[0].style.filter('invert(1)');    
+                // imgsReseau[1].style.filter('invert(1)');    
+                // imgsReseau[2].style.filter('invert(1)');    
+                 }
+            else {
+                // passage en noir
+                root.style.setProperty('--couleur-secondaire','#fff');
+                root.style.setProperty('--couleur-principale','#000');
+                root.style.setProperty('--couleur-ombre','rgba(255,255,255,0.1)');
+                logo.style.filter = 'invert(0)'; 
+                // imgsReseau[0].style.filter('invert(0)');    
+                // imgsReseau[1].style.filter('invert(0)');    
+                // imgsReseau[2].style.filter('invert(0)');  
+            }
+            // switchBarre.classList.toggle('switched');
+            switchCercle.classList.toggle('switched');
+            
+        })
+
+        //------------- changement de langue :
+
+        const btnFr = document.getElementById('lang-fr');
+        const btnEn = document.getElementById('lang-en');
+
+        function changerLangue() {
+            // changer couleur bouton selectionné/ non selectionné
+            if(this === btnFr) {
+                this.classList.remove('langue-non-selectionnee');
+                btnEn.classList.add('langue-non-selectionnee');
+            }
+            if(this === btnEn) {
+                this.classList.remove('langue-non-selectionnee');
+                btnFr.classList.add('langue-non-selectionnee');
+            }
+        }
+        btnFr.addEventListener('click',changerLangue);
+        btnEn.addEventListener('click',changerLangue);
+        
+        
+}
